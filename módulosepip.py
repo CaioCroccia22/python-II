625d84ea265c6fb2c754ca1e178bcc778d648fc3
caiocroccia22 <caiocroccia22@gmail.com>
2023-11-07 21:35:13 -0300

    importação do módulo

--- módulosepip.py
+++ módulosepip.py
@@ -10,18 +10,3 @@ Isolamento no projeto de software
 
 """
 
-def sum(a,b):
-    result = a + b
-    return result
-
-def sub(a, b):
-    result = a - b
-    return result
-
-def mult(a, b):
-    result = a * b
-    return result
-
-def div(a, b):
-    result = a / b
-    return result
