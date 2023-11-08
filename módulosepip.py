1ae501262b3cdf9ffd34495e93f827c17077c503
caiocroccia22 <caiocroccia22@gmail.com>
2023-11-07 21:34:30 -0300

    Criação do módulo

---
+++ módulosepip.py
@@ -0,0 +1,27 @@
+"""
+Módulos  - São arquivos em python que pode conter funções classes e variáveis
+Eles podem ser utilizados via importação (import)
+Eles também permitem a reutilização de código
+"""
+"""
+Suas vantagens são a simplicidade, manutenbilidade no código
+Um programa não precisa ser uma entidade única(monolito).
+Isolamento no projeto de software
+
+"""
+
+def sum(a,b):
+    result = a + b
+    return result
+
+def sub(a, b):
+    result = a - b
+    return result
+
+def mult(a, b):
+    result = a * b
+    return result
+
+def div(a, b):
+    result = a / b
+    return result
