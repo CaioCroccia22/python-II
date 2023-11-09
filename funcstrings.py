7a8ea6ab94323505db35167d40fa39c8331abee1
caiocroccia22 <caiocroccia22@gmail.com>
2023-11-09 19:09:44 -0300

    Criação do modulo de string

---
+++ funcstrings.py
@@ -0,0 +1,8 @@
+def reverse(word):
+    return word[::-1]
+
+def even(word):
+   return word[::2]
+
+def odd(word):
+    return word[::1]
--- modulos.py
+++ modulos.py
@@ -11,7 +11,21 @@ Isolamento no projeto de software
 """
 
 import calc
+# from calc import div
+
 print(calc.sum(10, 2))
 print(calc.sub(3, 5))
 print(calc.mult(3, 5))
 print(calc.div(10, 5))
+
+"""
+Módulo string 
+Escrever um modulo em python para tratar algumas string que possua as seguintes funcionalidades:
+1- Inverter apenas letras com indice par
+2- Retornar apenas letras com indice par
+3- Retornar apenas letras com indice impar
+"""
+
+import funcstrings
+
+print(odd("Cachorro"))
