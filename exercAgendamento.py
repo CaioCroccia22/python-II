6426cdb89e5d57290aaa58a2421ba0b0efd8ee87
caiocroccia22 <caiocroccia22@gmail.com>
2023-11-23 14:08:56 -0300

    Anotações

--- exercAgendamento.py
+++ exercAgendamento.py
@@ -1,7 +1,7 @@
 """
 Agendamento de desligamento em python
 -> Crie duas funções em python para agendar o desligamento do 
-computador em uma hora e meia hora.
+computador em uma hora.
 """
 
 import os
@@ -11,8 +11,17 @@ shutdown = input("Deseja desligar o computador? (sim/não):")
 
 def shutdown_computer():
     if shutdown == 'sim':
-        os.system("shutdown /s /t 1")
+        os.system("shutdown /s /t 3600")
     if shutdown == 'não':
         exit()
 
 shutdown_computer()
+
+#Desligar o computador em 1 minuto
+os.system("shutdown /s") 
+
+#Desligar o computador agora
+os.system("shutdown /s /t 0")
+
+#Cancelar o desligamento
+os.system("shutdown /a")
