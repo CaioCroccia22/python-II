3bfadf7dcf31ffd7d26149813b6f5095b387df99
caiocroccia22 <caiocroccia22@gmail.com>
2023-11-23 20:46:26 -0300

    Módulo HTTP

---
+++ modhttserver.py
@@ -0,0 +1,18 @@
+"""
+
+Módulo HTTP - 
+
+Tem como objetivo simular um servidor web HTTP.
+Um servidor web HTTP permite duas maquinas se conectar
+Uma cliente e outro servidor
+O cliente vai requisitar algo para a maquina servidora delineado por metodos HTTP
+Basicamente permite que duas maquinas consigam se conectar
+Metodo get - Buscar informação em um servidor
+Metodo post - Adicionar informações a um servidor
+Metodo put - Atualiza informações a um servidor
+Metodo delete - Deleta informações de um servidor
+
+"""
+
+
+
