a98858818ad596224a00c4f4d719472319d5f71d
caiocroccia22 <caiocroccia22@gmail.com>
2023-11-27 22:22:39 -0300

    Aplicar a mediana

--- modstatisc.py
+++ modstatisc.py
@@ -1,4 +1,5 @@
 import statistics
+
 """
 
 
@@ -6,6 +7,9 @@ import statistics
 """
 
 # 1 - Aplicar a média
-print(statistics.mean(3, 2, 5, 8, 9))
+print(statistics.mean([3, 2, 5, 8, 9]))
 
-# 2 - 
+# 2 - Aplicar a mediana 
+print(statistics.median([1, 2, 4, 5, 6, 3, 5, 4]))
+
+# 3 - 
