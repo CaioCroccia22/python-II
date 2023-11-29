d98ce714b9ad9fca9a50040819b36661ad6c5062
caiocroccia22 <caiocroccia22@gmail.com>
2023-11-29 17:01:42 -0300

    Módulo Regex I

---
+++ modregex.py
@@ -0,0 +1,21 @@
+"""
+Módulo Regex
+
+Analisa com dados textuais, quando é citado a palavra dados
+logo vem a mente o big data, que seria o grande conjunto de dados
+sendo boa parte do dados de big data sendo os dados não estruturados
+sendo a maioria dados textuais
+
+"""
+
+import re
+
+text = "Onebitcode: Transformamos pessoas em programadores sem limites"
+
+
+# 1 - Indice  inicial e final da palavras
+# O "r" significa que estamos trabalhando com uma string bruta (row string)
+match = re.search(r'pessoas em programadores',text) #Procure a frase para mim na variável text
+
+print(f'indice inicial {match.start()}')
+print(f'indice final {match.end()}')
--- modstatisc.py
+++ modstatisc.py
@@ -2,7 +2,7 @@ import statistics
 
 """
 
-
+Biblioteca state model
 
 """
 
@@ -22,3 +22,4 @@ Quanto mais próximo de 0 for o desvio padrão
 significa que os dados do conjunto estão menos dispersos
 """
 print(statistics.stdev([1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]))
+
