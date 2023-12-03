51d29b4a30bd8f752eb21ac56374bb09cadb48c6
caiocroccia22 <caiocroccia22@gmail.com>
2023-12-03 20:32:53 -0300

    Utilizando o MD5

--- modhashlib.py
+++ modhashlib.py
@@ -1,15 +1,19 @@
 """
 Modulo HashLib 
 Tem por objetivo criptografar textos
+Python tem diversos modulos e bibliotecas externas para trabalhar com a segurança da informação
+Utilizar o python para trabalhar com a criptografia
+
 
 """
 
 import hashlib
 
 # #Verificar os algoritmos disponiveis
+
 # print(hashlib.algorithms_available)
 # """
-# {'sha3_224', 'sha512_256', 'sha256', 'sha384', 'sha512', 'sha1', 'shake_2o\Python\Módulo 2>56', 'blake2s', 'sha224', 'blake2b', 'shake_128', 'sha3_512', 'sha3_256', 'sm3', 'sha512_224', 'ripemd160', 'sha3_384', 'md5', 'md5-sha1'} 
+#return => {'sha3_224', 'sha512_256', 'sha256', 'sha384', 'sha512', 'sha1', 'shake_2o\Python\Módulo 2>56', 'blake2s', 'sha224', 'blake2b', 'shake_128', 'sha3_512', 'sha3_256', 'sm3', 'sha512_224', 'ripemd160', 'sha3_384', 'md5', 'md5-sha1'} 
 # """
 
 # #Algoritmos disponiveis de acordo com o SO
@@ -20,4 +24,10 @@ algorithm = hashlib.sha256()
 print(algorithm.digest())
 message = "O caio é muito legal".encode()
 algorithm.update(message)
-print(algorithm.hexdigest())
+print(algorithm.hexdigest())#Texto criptografado em hexadecimal
+
+
+#Utilizando o MD5
+md5 = hashlib.md5()
+md5.update(message)
+print(md5.hexdigest())
