3afcf19370570a13dd32d2ddb0653e6addbdcc7d
caiocroccia22 <caiocroccia22@gmail.com>
2023-12-03 21:35:08 -0300

    Salvando json em txt

--- modjson.py
+++ modjson.py
@@ -21,3 +21,14 @@ print(person_dicti)
 person_json = json.dumps(person_dicti)#dumps - serializa um objeto python para uma string json
 print(person_json)
 print(type(person_json))
+print(type(person_dicti))
+
+# 3 - Formatando Json
+print(json.dumps(person_dicti,indent=4, sort_keys=True ))
+#O parâmetro indent=4 é usado para especificar a indentação de 4 espaços na string JSON, tornando-a mais legível para os humanos.
+#O parâmetro sort_keys=True indica que as chaves do dicionário devem ser ordenadas em ordem alfabética na string JSON resultante.
+#Essa operação é útil ao exibir ou armazenar dados JSON de forma organizada e legível
+
+# 4 - Salvando json em txt
+with open("person.txt", "w") as json_file:
+    json.dump(person_dicti, json_file)
