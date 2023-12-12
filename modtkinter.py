9ba119c092d2fd3e49c3f449584fb381bbb4545f
caiocroccia22 <caiocroccia22@gmail.com>
2023-12-11 22:00:05 -0300

    Adicionando o frame

--- modtkinter.py
+++ modtkinter.py
@@ -6,4 +6,17 @@ window = tkinter.Tk()
 window.geometry("300x150")
 window.title("Gerenciador de frases")
 
+
+# 2 - Adicionando o Frame
+frame = tkinter.Frame(window)
+frame.pack(padx=10, pady=10, fill="x", expand=True)
+
+# 3 - Adicionando o label
+label = tkinter.Label(frame, text="Hello World!!")
+label.pack(fill='x', expand=True)
+
+# 4 - Adicionando o input text
+frase_lab = tkinter.Label()
+
 window.mainloop()
+
